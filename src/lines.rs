35551a5380c64a27e38ad2f642826d698fd8701e
Roger Curley <rocurley@gmail.com>
2020-10-08 11:34:38 -0400

    Most of the line-wise rendering code done

--- src/lines.rs
+++ src/lines.rs
@@ -1,10 +1,39 @@
 use serde_json::value::{Number, Value};
+use tui::{
+    style::{Color, Modifier, Style},
+    text::Span,
+};
 
 #[derive(Debug, Clone)]
 pub struct Line {
     pub content: LineContent,
     pub key: Option<String>,
     pub folded: bool,
+}
+
+impl Line {
+    fn is_closing(&self) -> bool {
+        match self.content {
+            LineContent::ArrayStart(_) => true,
+            LineContent::ObjectEnd(_) => true,
+            _ => false,
+        }
+    }
+    fn next_displayed_offset(&self) -> usize {
+        match self {
+            Line {
+                content: LineContent::ArrayStart(lines_skipped),
+                folded: true,
+                ..
+            } => lines_skipped + 2,
+            Line {
+                content: LineContent::ObjectStart(lines_skipped),
+                folded: true,
+                ..
+            } => lines_skipped + 2,
+            _ => 1,
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -17,15 +46,16 @@ pub enum LineContent {
     ArrayEnd(usize),
     ObjectStart(usize),
     ObjectEnd(usize),
-    End,
 }
 
-pub fn jsons_to_lines<'a, I: Iterator<Item = &'a Value>>(vs: I) -> Vec<Line> {
-    let mut out = Vec::new();
-    for value in vs {
-        json_to_lines_inner(None, value, &mut out);
-    }
-    out
+pub fn json_to_lines<'a, I: Iterator<Item = &'a Value>>(vs: I) -> Vec<Vec<Line>> {
+    vs.into_iter()
+        .map(|value| {
+            let mut out = Vec::new();
+            json_to_lines_inner(None, value, &mut out);
+            out
+        })
+        .collect()
 }
 
 fn push_line(key: Option<String>, content: LineContent, out: &mut Vec<Line>) {
@@ -64,7 +94,7 @@ fn json_to_lines_inner(key: Option<String>, v: &Value, out: &mut Vec<Line>) -> u
             }
             push_line(None, LineContent::ArrayEnd(count), out);
             out[start_position].content = LineContent::ArrayStart(count);
-            count
+            count + 2
         }
         Value::Object(xs) => {
             let mut count = 0;
@@ -75,7 +105,153 @@ fn json_to_lines_inner(key: Option<String>, v: &Value, out: &mut Vec<Line>) -> u
             }
             push_line(None, LineContent::ArrayEnd(count), out);
             out[start_position].content = LineContent::ArrayStart(count);
-            count
+            count + 2
         }
+    }
+}
+
+struct JsonText<'a> {
+    indent: usize,
+    lines: &'a [Line],
+    cursor: usize,
+    i: usize,
+}
+impl<'a> Iterator for JsonText<'a> {
+    type Item = Vec<Span<'a>>;
+    fn next(&mut self) -> Option<Vec<Span<'a>>> {
+        let line = self.lines.get(self.i)?;
+        let next = self.lines.get(self.i + 1);
+        let has_comma = match next {
+            None => false,
+            Some(line) => line.is_closing(),
+        };
+        let indent_span = Span::raw("  ".repeat(self.indent));
+        let mut out = match &line.key {
+            Some(key) if !line.is_closing() => vec![
+                indent_span,
+                Span::raw(format!("{:?}", key)),
+                Span::raw(" : "),
+            ],
+            _ => vec![indent_span],
+        };
+        let style = if self.i == self.cursor {
+            Style::default().bg(Color::Blue)
+        } else {
+            Style::default()
+        };
+        match line {
+            Line {
+                content: LineContent::Null,
+                ..
+            } => {
+                out.push(Span::styled("null", style));
+                if has_comma {
+                    out.push(Span::raw(","));
+                }
+            }
+            Line {
+                content: LineContent::String(s),
+                ..
+            } => {
+                out.push(Span::styled(format!("{:?}", s), style));
+                if has_comma {
+                    out.push(Span::raw(","));
+                }
+            }
+            Line {
+                content: LineContent::Bool(b),
+                ..
+            } => {
+                out.push(Span::styled(b.to_string(), style));
+                if has_comma {
+                    out.push(Span::raw(","));
+                }
+            }
+            Line {
+                content: LineContent::Number(x),
+                ..
+            } => {
+                out.push(Span::styled(x.to_string(), style));
+                if has_comma {
+                    out.push(Span::raw(","));
+                }
+            }
+            Line {
+                content: LineContent::ArrayStart(skipped_lines),
+                folded: true,
+                ..
+            } => {
+                out.push(Span::styled("[...]", style));
+                if has_comma {
+                    out.push(Span::raw(","));
+                }
+                out.push(Span::styled(
+                    format!(" ({} lines)", skipped_lines),
+                    Style::default().add_modifier(Modifier::DIM),
+                ));
+            }
+            Line {
+                content: LineContent::ArrayEnd(_),
+                folded: true,
+                ..
+            } => {
+                panic!("Attempted to print close of folded array");
+            }
+            Line {
+                content: LineContent::ArrayStart(_),
+                folded: false,
+                ..
+            } => {
+                out.push(Span::styled("[", style));
+                self.indent += 1;
+            }
+            Line {
+                content: LineContent::ArrayEnd(_),
+                folded: false,
+                ..
+            } => {
+                out.push(Span::styled("]", style));
+                self.indent -= 1;
+            }
+            Line {
+                content: LineContent::ObjectStart(skipped_lines),
+                folded: true,
+                ..
+            } => {
+                out.push(Span::styled("{...}", style));
+                if has_comma {
+                    out.push(Span::raw(","));
+                }
+                out.push(Span::styled(
+                    format!(" ({} lines)", skipped_lines),
+                    Style::default().add_modifier(Modifier::DIM),
+                ));
+            }
+            Line {
+                content: LineContent::ObjectEnd(_),
+                folded: true,
+                ..
+            } => {
+                panic!("Attempted to print close of folded array");
+            }
+            Line {
+                content: LineContent::ObjectStart(_),
+                folded: false,
+                ..
+            } => {
+                out.push(Span::styled("{", style));
+                self.indent += 1;
+            }
+            Line {
+                content: LineContent::ObjectEnd(_),
+                folded: false,
+                ..
+            } => {
+                out.push(Span::styled("}", style));
+                self.indent -= 1;
+            }
+        };
+        self.i += line.next_displayed_offset();
+        Some(out)
     }
 }
