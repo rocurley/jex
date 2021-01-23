6a5f708ace934e5e36fe39e0f81cc457a545c31b
Roger Curley <rocurley@gmail.com>
2021-01-23 15:30:11 -0500

    Commit layout.rs

---
+++ src/layout.rs
@@ -0,0 +1,44 @@
+use tui::layout::{Constraint, Direction, Layout, Rect};
+#[derive(Clone, Copy, Debug)]
+pub struct JexLayout {
+    pub tree: Option<Rect>,
+    pub left: Rect,
+    pub right: Rect,
+    pub query: Rect,
+}
+
+impl JexLayout {
+    pub fn new(size: Rect, show_tree: bool) -> JexLayout {
+        let vchunks = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints([Constraint::Min(0), Constraint::Length(1)].as_ref())
+            .split(size);
+        if show_tree {
+            let tree_split = Layout::default()
+                .direction(Direction::Horizontal)
+                .constraints([Constraint::Length(20), Constraint::Ratio(1, 1)].as_ref())
+                .split(vchunks[0]);
+            let views = Layout::default()
+                .direction(Direction::Horizontal)
+                .constraints([Constraint::Ratio(1, 2), Constraint::Ratio(1, 2)].as_ref())
+                .split(tree_split[1]);
+            JexLayout {
+                tree: Some(tree_split[0]),
+                left: views[0],
+                right: views[1],
+                query: vchunks[1],
+            }
+        } else {
+            let views = Layout::default()
+                .direction(Direction::Horizontal)
+                .constraints([Constraint::Ratio(1, 2), Constraint::Ratio(1, 2)].as_ref())
+                .split(vchunks[0]);
+            JexLayout {
+                tree: None,
+                left: views[0],
+                right: views[1],
+                query: vchunks[1],
+            }
+        }
+    }
+}
